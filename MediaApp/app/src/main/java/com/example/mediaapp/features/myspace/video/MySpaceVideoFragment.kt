a66c224c654b4524c099807ade4d21db82e76286
quocvuphamdinh <vuphamdinh02@gmail.com>
2022-05-07 22:12:57 +0700

    hi

--- MediaApp/app/src/main/java/com/example/mediaapp/features/myspace/video/MySpaceVideoFragment.kt
+++ MediaApp/app/src/main/java/com/example/mediaapp/features/myspace/video/MySpaceVideoFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mediaapp.R
 import com.example.mediaapp.databinding.FragmentVideoMySpaceBinding
 import com.example.mediaapp.features.myspace.adapters.MySpaceMusicAdapter
 import com.example.mediaapp.models.Directory
@@ -47,7 +49,7 @@ class MySpaceVideoFragment : Fragment() {
     private fun setUpRecyclerViewFile() {
         fileAdapter = MySpaceMusicAdapter(object : MySpaceMusicAdapter.CLickItemDirectory{
             override fun clickItem(directory: Directory) {
-
+                findNavController().navigate(R.id.action_mySpaceFragment_to_videoDetailFragment)
             }
         })
         fileAdapter.submitList(DataStore.getListDirectory())
